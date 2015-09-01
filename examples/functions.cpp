dcc9048265f965a45d1f31c97ee7fbe3c18eaabb
sc_carlito831 <dannyescobedo91@gmail.com>
2015-09-01 22:51:40 +0000

    reorganizing directory

---
+++ examples/functions.cpp
@@ -0,0 +1,5 @@
+#include "functions.h"
+
+void goodbye () {
+    cout << "Hola!" << endl;
+}
---
+++ examples/hello.cpp
@@ -0,0 +1,13 @@
+#include "functions.h"
+
+#include <iostream>
+using namespace std;
+
+int main (int argc, char **argv) {
+    
+    cout << "Hello world!" << endl;
+    
+    goodbye();
+    
+    return 0;
+}
